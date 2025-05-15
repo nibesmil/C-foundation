a3f7f987b878f8184bd2f7987c3d67a585e5d2c5
lilsebinvert <carefox1@naver.com>
2025-05-15 10:15:11 +0900

    포인터 기초1

---
+++ pointerbasic1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int i = 10;
+	char c = 69;
+	float f = 12.3;
+
+	printf("i 주소값 : %p\n", &i);
+	printf("c 주소값 : %p\n", &c);
+	printf("f 주소값 : %p\n", &f);
+
+}
